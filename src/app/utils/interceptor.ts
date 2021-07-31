d6d3fc9dec5392e2154b7e350ca70cacc16656f4
Farzam <mohammad.malekzad@yahoo.com>
2021-07-31 23:13:35 +0430

    token interceptor

--- src/app/utils/interceptor.ts
+++ src/app/utils/interceptor.ts
@@ -1,12 +1,22 @@
 import {HttpHandler, HttpInterceptor, HttpRequest, HttpEvent} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {DomainUrl} from "./domain";
+import {CookieService} from "ngx-cookie-service";
+import {Injectable} from "@angular/core";
+
+@Injectable({
+  providedIn: 'root'
+})
 
 export class interceptor implements HttpInterceptor {
+  constructor(public cookieService: CookieService) {
+  }
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token: string = this.cookieService.get('admin-cookie');
     const myRequest = req.clone({
       url: DomainUrl + req.url,
-      headers: req.headers.append('Content-Type', 'application/json')
+      headers: req.headers.append('Authorization', 'bearer ' + token)
     });
 
     return next.handle(myRequest);
